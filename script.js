213d6b80eaec9f5369a084bcd46d6a411df55b6f
Lilian CHAPTAL <chaptal.lilian@gmail.com>
2023-06-05 10:13:05 +0200

    Initial commit

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ dropdownItems.forEach(item => {
     item.querySelector('.dropdown-menu').classList.remove('show');
   });
 });
+
+var menuItems = document.querySelectorAll('.menu li a');
+
+menuItems.forEach(function(item) {
+  item.addEventListener('mouseover', function() {
+    this.style.color = '#00ff00'; /* Couleur verte au survol */
+  });
+
+  item.addEventListener('mouseout', function() {
+    this.style.color = '#333'; /* Retour à la couleur initiale */
+  });
+});
